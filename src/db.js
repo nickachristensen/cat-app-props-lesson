03c764ea40d434af74c94dfd46d4f927518fba97
Nicholas A Christensen <nickachristensen@gmail.com>
2022-11-29 17:25:05 -0500

    created database

---
+++ src/db.js
@@ -0,0 +1,71 @@
+let cats = [
+    {
+        name: "Stevie",
+        color: "Black",
+        personality: "Shy and playful",
+        image: "https://media-be.chewy.com/wp-content/uploads/2016/09/01160419/black-cat-1-1024x548.jpg",
+        toys: [
+            {
+                type:"Wand toy",
+                hasCatnip: false,
+                excitmentRating: 9,
+            },
+            {
+                type:"Fox",
+                hasCatnip: true,
+                excitmentRating: 7,
+            },
+            {
+                type:"Laser pointer",
+                hasCatnip: false,
+                excitmentRating: 10,
+            },
+        ],
+    },
+    {
+        name: "Higgins",
+        color: "Black and white",
+        personality: "Outgoing and cuddly",
+        image: "https://coleandmarmalade.com/wp-content/uploads/2019/09/Canva-Black-and-White-Cat-e1569611884531.jpg",
+        toys: [
+            {
+                type:"Wand toy",
+                hasCatnip: false,
+                excitmentRating: 6,
+            },
+            {
+                type:"Fox",
+                hasCatnip: true,
+                excitmentRating: 3,
+            },
+            {
+                type:"Laser pointer",
+                hasCatnip: false,
+                excitmentRating: 10,
+            },
+        ],
+    },
+    {
+        name: "Pearl",
+        color: "White",
+        personality: "Shy and cuddly",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Khaomanee_cat.jpg",
+        toys: [
+            {
+                type:"Wand toy",
+                hasCatnip: false,
+                excitmentRating: 7,
+            },
+            {
+                type:"Fox",
+                hasCatnip: true,
+                excitmentRating: 5,
+            },
+            {
+                type:"Laser pointer",
+                hasCatnip: false,
+                excitmentRating: 10,
+            },
+        ],
+    },
+]
